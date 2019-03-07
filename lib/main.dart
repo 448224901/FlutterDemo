a35643ab50c5ddcb881dcd61c84fe5fa72c18150
wwj <448224901@qq.com>
2019-03-07 16:10:39 +0800

    修改文件

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'demo.dart';
 
-void main() => runApp(new MyApp());
+//void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
